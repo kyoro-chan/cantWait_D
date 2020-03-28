0e204729dc58dc408e93cdd4b2bdab0ddb229b03
kyoro-chan <61698254+kyoro-chan@users.noreply.github.com>
2020-03-28 20:32:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -23,11 +23,13 @@ $(function(){
     //ボタン押下時の処理
     $('#btn-character').click(function(){
         
+        count ++;
+
         //画面の幅を取得
         var windowWidth = $(window).width();
         var minWidth = 1000
 
-        if(count > 14){
+        if(count > 15){
             //画面の幅に対応する背景を設定
             if(windowWidth > minWidth){
                 $('.back').css('background-image', 'url(castle_forPC.jpg)').css('background-size', 'cover')
@@ -38,67 +40,70 @@ $(function(){
             //文字の色、テキストを変更
             $('.title-date').css('color', 'yellow')
             $('.btn').text('行きたいね！')
+            $('.push-count').text(count + '行きたいよー！').css('color', 'seashell')
+        }else{
+            $('.push-count').text(count + '行きたい！')
         }
            
         //ボタン押下ごとにキャラクターを表示する
         switch(count){
-            case 0:
+            case 1:
                 $('.pluto').fadeIn(250);
                 break;
 
-            case 1:
+            case 2:
                 $('.mickey').fadeIn(300);
                 break;
 
-            case 2:
+            case 3:
                 $('.minnie').fadeIn(300);
                 break;
 
-            case 3:
+            case 4:
                 $('.donald').fadeIn(300);
                 break;
 
-            case 4:
+            case 5:
                 $('.goofy').fadeIn(300);
                 break;
                 
-            case 5:
+            case 6:
                 $('.chipndale').fadeIn(300);
                 break;
 
-            case 6:
+            case 7:
                 $('.dasy').fadeIn(300);
                 break;
 
-            case 7:
+            case 8:
                 $('.bell').fadeIn(300);
                 break;
                 
-            case 8:
+            case 9:
                 $('.chindlella').fadeIn(300);
                 break;
                 
-            case 9:
+            case 10:
                 $('.alice').fadeIn(300);
                 break;
 
-            case 10:
+            case 11:
                 $('.roger').fadeIn(300);
                 break;
 
-            case 11:
+            case 12:
                 $('.smee').fadeIn(300);
                 break;
                 
-            case 12:
+            case 13:
                 $('.melida').fadeIn(300);
                 break;
 
-            case 13:
+            case 14:
                 $('.ariel').fadeIn(300);
                 break;
                 
-            case 14:
+            case 15:
                 $('.carpet').fadeIn(300);
                 break;
 			
@@ -107,6 +112,5 @@ $(function(){
 			    break;
         }
 
-        count ++;
     });
 });
