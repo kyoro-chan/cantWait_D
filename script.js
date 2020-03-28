c3486069ea108758d55aa341aa677b0dafced67e
kyoro-chan <61698254+kyoro-chan@users.noreply.github.com>
2020-03-28 11:40:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ $(function(){
         $('.title-date').text('再開園まで後　' + leastDay + '日');
     }
 
-
     //ボタン押下回数をカウント
     var count = 0;
 
@@ -28,29 +27,81 @@ $(function(){
         var windowWidth = $(window).width();
         var minWidth = 1000
 
-        if(count > 1){
+        if(count > 13){
             //画面の幅に対応する背景を設定
             if(windowWidth > minWidth){
                 $('.back').css('background-image', 'url(castle_forPC.jpg)').css('background-size', 'cover')
             }else{
                 $('.back').css('background-image', 'url(castle.jpg)').css('background-size', 'cover')
             }
+
             //文字の色、テキストを変更
-            $('.title-date').css('color', 'navy')
+            $('.title-date').css('color', 'yellow')
             $('.btn').text('行きたいね！')
-            
-        }/*else{
+        }
            
-            
-            }
-        }*/
-        
+        //ボタン押下ごとにキャラクターを表示する
+        switch(count){
+            case 0:
+                $('.pluto').fadeIn(250);
+                break;
 
+            case 1:
+                $('.mickey').fadeIn(300);
+                break;
 
+            case 2:
+                $('.minnie').fadeIn(300);
+                break;
 
+            case 3:
+                $('.donald').fadeIn(300);
+                break;
 
+            case 4:
+                $('.goofy').fadeIn(300);
+                break;
+                
+            case 5:
+                $('.chipndale').fadeIn(300);
+                break;
 
+            case 6:
+                $('.dasy').fadeIn(300);
+                break;
 
+            case 7:
+                $('.alice').fadeIn(300);
+                break;
+                
+            case 8:
+                $('.chindlella').fadeIn(300);
+                break;
+
+            case 9:
+                $('.roger').fadeIn(300);
+                break;
+
+            case 10:
+                $('.smee').fadeIn(300);
+                break;
+                
+            case 11:
+                $('.melida').fadeIn(300);
+                break;
+
+            case 12:
+                $('.ariel').fadeIn(300);
+                break;
+                
+            case 13:
+                $('.carpet').fadeIn(300);
+                break;
+			
+		    default:
+			    $('.mickey').fadeIn(300);
+			    break;
+        }
 
         count ++;
     });
